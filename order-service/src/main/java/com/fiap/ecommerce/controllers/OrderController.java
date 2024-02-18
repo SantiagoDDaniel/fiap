fcf07e3731fc29f25c0e29f36abefb7bd033e9fb
Daniel Santiago <daniel.santiagoti@hotmail.com>
2024-02-18 11:28:42 -0300

    Resolvido o problema do cart e order não aparecer na instance do eureka

---
+++ order-service/src/main/java/com/fiap/ecommerce/controllers/OrderController.java
@@ -0,0 +1,45 @@
+package com.fiap.ecommerce.controllers;
+
+import com.fiap.ecommerce.components.OrderDetails;
+import com.fiap.ecommerce.entities.Order;
+import com.fiap.ecommerce.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    private final OrderService orderService;
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("")
+    public List<OrderDetails> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/{id}")
+    public Order getOrderById(@PathVariable Long id) {
+        return orderService.getOrderById(id);
+    }
+
+    @PostMapping("")
+    public Order createOrder(@RequestBody Order product) {
+        return orderService.createOrder(product);
+    }
+
+    @PutMapping("/{id}")
+    public Order updateOrder(@PathVariable Long id, @RequestBody Order product) {
+        return orderService.updateOrder(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOrder(@PathVariable Long id) {
+        orderService.deleteOrder(id);
+    }
+}
+
