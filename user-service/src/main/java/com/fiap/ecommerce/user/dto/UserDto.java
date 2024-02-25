8ba3f2f14889035a7c38909379f99139fea410ca
kmentz <kassiane.mentz@adp.com>
2024-02-25 17:18:33 -0300

    adicionando validacao no UserDto

--- user-service/src/main/java/com/fiap/ecommerce/user/dto/UserDto.java
+++ user-service/src/main/java/com/fiap/ecommerce/user/dto/UserDto.java
@@ -2,8 +2,16 @@ package com.fiap.ecommerce.user.dto;
 
 
 import com.fiap.ecommerce.user.model.RoleEnum;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-public record UserDto(String name, String login, String password, RoleEnum role) {
+public record UserDto (
+        @NotBlank(message = "Nome  Obrigatorio") String name,
+        @NotBlank(message = "Login Obrigatorio") @Email(message = "Login should be an email") String login,
+        @NotBlank(message = "Senha Obrigatoria") @Size(min = 6, message = "Senha deve ter no minino 6 caracteres") String password,
+        @NotNull(message = "Role Obrigatoria") RoleEnum role) {
     public UserDto(String name, String login, String password, RoleEnum role) {
         this.name = name;
         this.login = login;
